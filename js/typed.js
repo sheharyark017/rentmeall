ae49521f5a6d1761910887ae1d589844b57e06ca
sheharyark017 <sheharyark017@gmail.com>
2022-09-06 11:23:11 +0500

    auth complete

---
+++ js/typed.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text-animation", {
+  strings: [
+    "Spaces",
+    "Vehicles",
+    "Furniture",
+    "Business",
+    "Tools",
+    "Equipments",
+  ],
+  typeSpeed: 150,
+  backSpeed: 50,
+  smartBackspace: true, // Default value
+  loop: true,
+});
